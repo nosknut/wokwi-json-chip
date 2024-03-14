3e57e1fb58b48df297afefc7e53278058a1b65c5
Yes <Yes>
2024-03-13 23:20:13 -0500

    moved chipInit to main1.rs added comment on main1.rs added comments to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,10 @@
+/*
+    Note, This should be considered almost a separate crate however i'm prototyping
+    For actual use please convert main1 to some form of example and keep redefine exports for the rest
+*/
+
 pub mod json_parser;
 pub mod uart;
 
-use json_parser::JsonParser;
-
-use uart::{debug_print_string, Uart};
-
-static mut PARSER: JsonParser = JsonParser::new();
-
-#[no_mangle]
-#[allow(clippy::missing_safety_doc)]
-pub unsafe extern "C" fn chipInit() {
-    Uart::init("TX", "RX", 115200, |uart, _c| {
-        if let Ok(Some(json)) = PARSER.parse_uart(uart) {
-            debug_print_string(format!("Received: {:?}", json));
-
-            let response = match json["topic"].as_str().unwrap() {
-                "servo/init" => {
-                    serde_json::json!({
-                        "topic": "servo/status",
-                        "position": 50
-                    })
-                }
-                "servo/target-position" => {
-                    serde_json::json!({
-                        "topic": "servo/status",
-                        "position": json["position"]
-                    })
-                }
-                _ => {
-                    serde_json::json!({
-                        "topic": "servo/error",
-                        "message": format!("Unknown topic: {}", json["topic"].as_str().unwrap_or("None").to_owned())
-                    })
-                }
-            };
-
-            debug_print_string(format!("Sent: {}", response));
-            uart.write_string(response.to_string());
-        }
-    });
-}
+mod main1;
+pub use main1::*;
---
+++ src/main1.rs
@@ -0,0 +1,45 @@
+/*
+    File is called main1 as it need to remain lib for chipInit()
+    main.rs would require a main however this runs off chipInit()
+*/
+
+use crate::{
+    json_parser::JsonParser,
+    uart::{debug_print_string, Uart},
+};
+
+static mut PARSER: JsonParser = JsonParser::new();
+
+#[no_mangle]
+#[allow(clippy::missing_safety_doc)]
+pub unsafe extern "C" fn chipInit() {
+    Uart::init("TX", "RX", 115200, |uart, _c| {
+        if let Ok(Some(json)) = PARSER.parse_uart(uart) {
+            debug_print_string(format!("Received: {:?}", json));
+
+            let response = match json["topic"].as_str().unwrap() {
+                "servo/init" => {
+                    serde_json::json!({
+                        "topic": "servo/status",
+                        "position": 50
+                    })
+                }
+                "servo/target-position" => {
+                    serde_json::json!({
+                        "topic": "servo/status",
+                        "position": json["position"]
+                    })
+                }
+                _ => {
+                    serde_json::json!({
+                        "topic": "servo/error",
+                        "message": format!("Unknown topic: {}", json["topic"].as_str().unwrap_or("None").to_owned())
+                    })
+                }
+            };
+
+            debug_print_string(format!("Sent: {}", response));
+            uart.write_string(response.to_string());
+        }
+    });
+}
